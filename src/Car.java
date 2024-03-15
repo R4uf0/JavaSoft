0a53cd45ca5e043d0dbeafcf9ebfdbce312b4ec4
R4uf0 <rauf3sedli20.06@gmail.com>
2024-03-16 00:03:13 +0400

    newcomment

---
+++ src/Car.java
@@ -0,0 +1,55 @@
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private static int maxSpeedLimit = 120;
+
+    // Constructor
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getter for maxSpeedLimit
+    public static int getMaxSpeedLimit() {
+        return maxSpeedLimit;
+    }
+
+    // Method to display car info
+    public void displayInfo() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    // Getter for make
+    public String getMake() {
+        return make;
+    }
+
+    // Setter for make
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    // Getter for model
+    public String getModel() {
+        return model;
+    }
+
+    // Setter for model
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    // Getter for year
+    public int getYear() {
+        return year;
+    }
+
+    // Setter for year
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.print("Hello and welcome!");
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of cars you want to create:");
+        int numCars = scanner.nextInt();
+
+        for (int i = 0; i < numCars; i++) {
+            System.out.println("Enter make for Car " + (i + 1) + ":");
+            String make = scanner.next();
+
+            System.out.println("Enter model for Car " + (i + 1) + ":");
+            String model = scanner.next();
+
+            System.out.println("Enter year for Car " + (i + 1) + ":");
+            int year = scanner.nextInt();
+
+            Car car = new Car(make, model, year);
+            car.displayInfo();
+            System.out.println();
+        }
+
     }
 }
